4dd92e74679f7b6b3a01f2c1cd4b2499a2ac742a
juananton <juananton.correo@gmail.com>
2022-09-25 17:08:14 +0200

    Create About page with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
 
 import FeedbackDataSource from './data/FeedbackDataSource';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
   const [feedbackData, setFeedbackData] = useState(FeedbackDataSource);
@@ -22,17 +25,29 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <div className='container'>
         <Header />
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedbackListData={feedbackData} />
-        <FeedbackList
-          feedbackListData={feedbackData}
-          handleDelete={deleteFeedbackItem}
-        />
+        <Routes>
+          <Route
+            exact
+            path='/'
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedbackListData={feedbackData} />
+                <FeedbackList
+                  feedbackListData={feedbackData}
+                  handleDelete={deleteFeedbackItem}
+                />
+                <AboutIconLink />
+              </>
+            }
+          ></Route>
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
