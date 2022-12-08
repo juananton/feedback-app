42cc73cbf4691861bc4f8c7094bda56d315c2a26
juananton <juananton.correo@gmail.com>
2022-12-08 14:53:15 +0100

    css fixes

--- src/pages/InfoPage.jsx
+++ src/pages/InfoPage.jsx
@@ -8,9 +8,8 @@ function InfoPage() {
         <h1>About this project</h1>
         <p>This is a React app to leave feedback for a product or service</p>
       </div>
-      <p>Version 1.0.0</p>
       <p>
-        <Link to='/'>Back to home</Link>
+        <Link to='/'>← Back to home</Link>
       </p>
     </Card>
   );
